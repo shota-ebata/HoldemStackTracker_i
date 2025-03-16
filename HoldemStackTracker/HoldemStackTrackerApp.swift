f697154a28110f42eb45dca5f53f258f758be61d
shota-ebata <maamaaomosiroi@gmail.com>
2025-03-16 23:34:49 +0900

    Initial Commit

---
+++ HoldemStackTracker/HoldemStackTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoldemStackTrackerApp.swift
+//  HoldemStackTracker
+//
+//  Created by 江幡将太 on 2025/03/16.
+//
+
+import SwiftUI
+
+@main
+struct HoldemStackTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HoldemStackTrackerTests/HoldemStackTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  HoldemStackTrackerTests.swift
+//  HoldemStackTrackerTests
+//
+//  Created by 江幡将太 on 2025/03/16.
+//
+
+import Testing
+@testable import HoldemStackTracker
+
+struct HoldemStackTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
